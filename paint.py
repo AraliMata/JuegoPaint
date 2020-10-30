0a7660a8663958824cea966d5541f9d2791afa5e
Yoceline Mata <aralipdf@gmail.com>
2020-10-29 17:12:25 -0700

    update paint.py
    Cambio en los comentarios

--- paint.py
+++ paint.py
@@ -1,16 +1,16 @@
 """Paint, for drawing shapes.
-
 Exercises
-
 1. Add a color.
 2. Complete circle.
 3. Complete rectangle.
 4. Complete triangle.
 5. Add width parameter.
-Hola qué tal, ¿cómo has estado?
 """
-#Holaaa soy Aralí
-#Wolaaa soy Laurie
+#Ejercicios hechos por:
+#Laurie Diego
+#Aralí Mata
+#Angel Corrales
+
 import math
 from turtle import *
 from freegames import vector
